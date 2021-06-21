401f58a87bf3dfbd4a31d2399907f392c653e40e
Vir00s <weglarzmateusz@gmail.com>
2021-06-21 22:05:58 +0200

    Dodaj pliki projektów.

---
+++ lab6.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void zmiana(int * wsk)
+{
+
+}
+
+int main()
+{
+	int** tabb = new int* [5];
+
+	for (int i = 0; i < 5; i++)
+		int* tab[i] = new int[i];
+
+
+
+	for (int i = 0; i < 5; i++)
+		delete[] tab[i];
+	delete[] tabb;
+
+	return 0;
+}
